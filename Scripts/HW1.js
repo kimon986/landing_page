d5b313af88d28459cccad387f30d15935aa4c6e8
kimon986 <kimonmono986@gmail.com>
2021-03-05 22:36:35 -0800

    finished mobile burger menu, and fixed features content with drop down text.

--- Scripts/HW1.js
+++ Scripts/HW1.js
@@ -1,4 +1,6 @@
+/*burger*/
 const burgerElement = document.querySelector('.burger__container')
+const closeElement = document.querySelector('.close__wrapper')
 
 const handleOpenMenu = () => {
   const nav = document.querySelector('.desktop_nav_wrapper')
@@ -6,15 +8,19 @@ const handleOpenMenu = () => {
 }
 
 burgerElement.addEventListener('click', handleOpenMenu)
+closeElement.addEventListener('click', handleOpenMenu)
 
 
-/*testing*/
+
+/*drop down text*/
 const dropdownHeaders = document.querySelectorAll('span.dropdown__header')
-const dropdownContents = document.querySelectorAll('.dropdown__content')
 
 for (let i = 0; i < dropdownHeaders.length; i++) {
-  dropdownHeaders[i].addEventListener('click', () => {
-    dropdownContents[i].classList.toggle('dropdown__content--active')
-  })
+  dropdownHeaders[i].addEventListener('click', () => showText(i)) //function, not return
 }
 
+const showText = (i) =>
+{
+  const dropdownContents = document.querySelectorAll('.dropdown__content')
+  dropdownContents[i].classList.toggle('dropdown__content--active')
+}
