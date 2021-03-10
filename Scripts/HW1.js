712eaa333f7e6947d43ec548499828974ce9c746
kimon986 <kimonmono986@gmail.com>
2021-03-09 18:31:08 -0800

    added slider functionality

--- Scripts/HW1.js
+++ Scripts/HW1.js
@@ -24,3 +24,36 @@ const showText = (i) =>
   const dropdownContents = document.querySelectorAll('.dropdown__content')
   dropdownContents[i].classList.toggle('dropdown__content--active')
 }
+
+/*slider*/
+const images = ["../images/castlebeckett.jpg", "../images/castlebeckett2.jpg",
+"../images/castlebeckett3.jpg"]
+
+let imgPosition = 0
+
+const changeImage = (position) => {
+  const imageElement = document.querySelector('.demo_wrapper > div:last-child > div')
+  
+  if (position > (images.length - 1)) {
+    imageElement.style.backgroundImage = `url("${images[0]}")`
+    imgPosition = 0
+    return
+  } else if (position < 0) {
+    const lastImage = images.length - 1
+    imageElement.style.backgroundImage = `url("${images[lastImage]}")`
+    imgPosition = lastImage
+    return
+  }
+  images.map((item, key) => {
+    if (key === position) {
+      imgPosition = position
+      imageElement.style.backgroundImage = `url("${item}")`
+    }
+  })
+}
+
+const left = document.getElementById('left-arrow')
+const right = document.getElementById('right-arrow')
+
+left.addEventListener('click', () => changeImage(imgPosition - 1))
+right.addEventListener('click', () => changeImage(imgPosition + 1))
